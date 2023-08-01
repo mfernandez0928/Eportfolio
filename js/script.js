f3b7b803bb21d0181227b6ad209aa81676041826
Mark Fernandez <mfernandez0928@gmail.com>
2023-08-01 18:20:30 +0800

    Update Navbar

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length;
+for (let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function () {
+        for (let j = 0; j < totalNavList; j++) {
+            navList[j].querySelector("a").classList.remove("active");
+        }
+        this.classList.add("active")
+    })
+}
