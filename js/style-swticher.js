1ea597648e627be05bdda549aa25bde1ce36da43
Mark Fernandez <mfernandez0928@gmail.com>
2023-08-01 15:12:11 +0800

    Updated my index and css

--- js/style-swticher.js
+++ js/style-swticher.js
@@ -1,19 +1,10 @@
-const styleSwitcherToggler = document.querySelector(".style-switcher-toggle");
-styleSwitcherToggler.addEventListener("click", () => {
-    if (document.querySelector(".style-switcher").classList.contains("open")) {
-        document.querySelector(".style-switcher").classList.remove("open");
-    }
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
 })
 
-const alternateStyle = document.querySelectorAll(".alternate-style");
-function setActiveStyle(color) {
-    localStorage.setItem("color", color);
-    changeColor();
-}
-function changeColor() {
-    alternateStyle.forEach((style) => {
-        if (localStorage.getItem("color") === style.getAttribute("title")) {
-            style.removeAttribute("disabled");
-        }
-    })
-}
+window.addEventListener("scroll", () => {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
+        document.querySelector(".style-switcher").classList.toggle("open");
+    }
+})
